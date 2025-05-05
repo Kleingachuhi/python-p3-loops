fd099582d4b730ffd9d3215a89075369017bc2da
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-05 16:40:17 +0300

    Python Conditionals

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+     for i in range(1, 11):
+         print(i)
+         i -=1
+         print("Happy New Year!")
+     pass
 
-def square_integers(int_list):
-    # code goes here!
-    pass
-
+def square_integers(int_list=[1, 2, 3, 4, 5] ):
+   int_list =[integer ** 2 for integer in int_list]
+   return(int_list)
+   pass
+square_integers()
 def fizzbuzz():
-    # code goes here!
+    i = 1
+    for i in range(1, 101):
+        if i %15 ==0 :
+            print("FizzBuzz")
+        elif i %5 ==0 :
+            print("Buzz")
+        elif i % 3 ==0:
+            print("Fizz") 
+        else:
+            print(i)
     pass
